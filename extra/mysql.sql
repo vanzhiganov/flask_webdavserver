f16eee88d6477deb3b14e8fde477f80003b48358
vanzhiganov <vanzhiganov@ya.ru>
2015-10-12 17:32:28 +0300

    update sql schema

--- extra/mysql.sql
+++ extra/mysql.sql
@@ -1,11 +1,11 @@
 CREATE DATABASE webdav;
 
-CREATE TABLE users(
- id INT NOT NULL AUTO_INCREMENT,
- email VARCHAR(100) NOT NULL,
- password VARCHAR(40) NOT NULL,
- PRIMARY KEY(id)
- );
-
+CREATE TABLE users (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  email varchar(128) NOT NULL,
+  password varchar(64) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY unique_email (email)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 INSERT INTO users (email, password) VALUES ('admin@admin.ru','admin');
